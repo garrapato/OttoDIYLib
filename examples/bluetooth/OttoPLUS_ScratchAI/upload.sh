9a8b566d56eef41b62ad4b6a51aa09fd8fea4955
Jorge <jorge@MacBook-Pro-de-Jorge.local>
2020-11-13 00:14:48 -0600

    Nuevo Estilo

--- examples/bluetooth/OttoPLUS_ScratchAI/upload.sh
+++ examples/bluetooth/OttoPLUS_ScratchAI/upload.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 BOARD=atmega328old
+#PORT=/dev/cu.wchusbserial14110
+PORT=/dev/cu.wchusbserial14310
+SCKETCH=OttoPLUS_ScratchAI
 
 #echo "Compiling..."
-#arduino-cli compile -v --fqbn arduino:avr:nano:cpu=$BOARD ${1}.ino
+#arduino-cli compile -v --fqbn arduino:avr:nano:cpu=$BOARD ${SCKETCH}.ino
+#arduino-cli compile -v --fqbn arduino:avr:nano:cpu=$BOARD ${SCKETCH}.ino
 echo "Uploading..."
-arduino-cli upload -v -p /dev/cu.wchusbserial14110 --fqbn arduino:avr:nano:cpu=$BOARD $1
+arduino-cli upload -v -p $PORT --fqbn arduino:avr:nano:cpu=$BOARD $SCKETCH
 echo "Done"
